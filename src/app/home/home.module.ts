374608a8dc16de8c8bb1570b677ead124bfe4751
Jair Suárez <jairjosesuarez8@gmail.com>
2022-01-29 13:16:59 -0500

    fix: Clean and adjust code

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
-
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+	declarations: [
+		HomePage
+	],
+	imports: [
+		CommonModule,
+		FormsModule,
+		IonicModule,
+		HomePageRoutingModule
+	]
 })
-export class HomePageModule {}
+export class HomePageModule { }
