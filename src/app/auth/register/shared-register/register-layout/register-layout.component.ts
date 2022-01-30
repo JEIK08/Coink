1a7d6e02cf52bbc590731b9ee7d00da0c3d2ddd0
Jair Suárez <jairjosesuarez8@gmail.com>
2022-01-29 19:46:21 -0500

    feat: Number pad design

--- src/app/auth/register/shared-register/register-layout/register-layout.component.ts
+++ src/app/auth/register/shared-register/register-layout/register-layout.component.ts
@@ -1,14 +1,20 @@
-import { Component, Input, TemplateRef } from '@angular/core';
+import { Component, HostBinding, Input, TemplateRef, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-register-layout',
 	templateUrl: './register-layout.component.html',
 	styleUrls: ['./register-layout.component.scss']
 })
-export class RegisterLayoutComponent {
+export class RegisterLayoutComponent implements OnInit {
+
+	@HostBinding('class') public class: string;
 
 	@Input() public iconPath: string;
-	@Input() public title: string;
+	@Input() public registerTitle: string;
 	@Input() public description: TemplateRef<void>;
+
+	ngOnInit() {
+		if (!this.registerTitle) this.class = 'without-title';
+	}
 
 }
